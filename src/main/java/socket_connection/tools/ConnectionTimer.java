577acd7331e08bc24984d016192c1c403cb77fca
polimi-hyp-2018-10497583 <andrea1.costanzo@mail.polimi.it>
2018-09-26 23:40:27 +0200

    Modified ConnectionTimer: now the TTL can be configured throught the jsons.

--- src/main/java/socket_connection/tools/ConnectionTimer.java
+++ src/main/java/socket_connection/tools/ConnectionTimer.java
@@ -11,13 +11,23 @@ public class ConnectionTimer {
 
     private SocketConnection connectionHandled;
     private long timePassedInSec;
-    private long timerSet=2; // FIXME: 12/09/2018
+    private long timerSet;
     private final Timer timer = new Timer();
+    private boolean running;
     private TimerTask connectionTimerTask;
     private final Lock lock=new ReentrantLock();
+    private final Lock timerLock= new ReentrantLock();
 
+
+    /**
+     * Public constructor for the timer
+     * @param connection is the connection controlled by the
+     *                   instance of the timer just created
+     */
     public ConnectionTimer(SocketConnection connection){
         this.connectionHandled=connection;
+        this.running=false;
+        timerSet=connection.getTimeToLive();
         connectionTimerTask = new TimerTask() {
             @Override
             public void run() {
@@ -25,21 +35,42 @@ public class ConnectionTimer {
                 long actualTime=++timePassedInSec;
                 lock.unlock();
                 if (actualTime >= timerSet) {
+                    timerLock.lock();
                     timer.cancel();
+                    timerLock.unlock();
                     connectionHandled.shutdown();
                 }
-
             }
         };
     }
 
+    /**
+     * This method resets timer "countdown"
+     */
     public void resetTTL(){
         lock.lock();
         timePassedInSec=0;
         lock.unlock();
     }
 
+    /**
+     * This method starts timer "countdown".
+     */
     public void launch(){
+        timerLock.lock();
         timer.scheduleAtFixedRate(connectionTimerTask,1,1000);
+        running=true;
+    }
+
+    /**
+     * This method is used to stop the timer.
+     */
+    public void stop() {
+        timerLock.lock();
+        if(running) {
+            timer.cancel();
+            running=false;
+        }
+        timerLock.unlock();
     }
 }

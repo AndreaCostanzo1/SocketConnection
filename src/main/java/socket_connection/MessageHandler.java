d3e15dcefa3c1b83bd59df25ede963e0db584fb8
polimi-hyp-2018-10497583 <andrea1.costanzo@mail.polimi.it>
2018-10-11 11:43:57 +0200

    Updated MessageHandler: Now message handler isn't a final class anymore: in fact now it's a normal class with static fields. This will be usefull when the cryptography will be implemented because a private key can be associated to each MessageHandler instance.

--- src/main/java/socket_connection/MessageHandler.java
+++ src/main/java/socket_connection/MessageHandler.java
@@ -5,9 +5,10 @@ import com.google.gson.Gson;
 import com.google.gson.JsonSyntaxException;
 import socket_connection.socket_exceptions.runtime_exceptions.UndefinedInputTypeException;
 import socket_connection.socket_exceptions.runtime_exceptions.BadSetupException;
+import socket_connection.tools.ConfigurationHandler;
+import socket_connection.tools.MessageHandlerConfigurations;
 
 import java.nio.charset.Charset;
-import java.nio.charset.StandardCharsets;
 import java.util.*;
 import java.util.function.Predicate;
 
@@ -16,31 +17,52 @@ interface DecodingFunction<T>{
     void run(T t);
 }
 
-final class MessageHandler {
+class MessageHandler {
 
+    private final Map<String, DecodingFunction<SocketConnection>> behavioursMap =new HashMap<>();
 
-    private static String dataMessage ="#DATA#";
-    private static Charset charset=StandardCharsets.UTF_16;
-    private static final String PING_MESSAGE="";
-    private static final String HELLO_MESSAGE="#HELLO#";
-    private static final String SERVER_IS_READY_MESSAGE = "#SERVER_READY#";
-
-    private static final Map<String, DecodingFunction<SocketConnection>> INPUT_TYPES=new HashMap<>();
-    static {
-        INPUT_TYPES.put(PING_MESSAGE, InputDecoder::handlePingMessage);
-        INPUT_TYPES.put(HELLO_MESSAGE, InputDecoder::handleHelloMessage);
-        INPUT_TYPES.put(SERVER_IS_READY_MESSAGE, InputDecoder::handleServerIsReadyMessage);
-    }
+    private static Charset charset;
+    private static String pingMessage;
+    private static String dataMessage;
+    private static String helloMessage;
+    private static String serverIsReadyMessage;
+    private static boolean configured=false;
     private static final int DATA_TAG_POSITION =0;
-    private static final Predicate<String> inputIsDataType =
-            s->s!=null && s.length()>=dataMessage.length() &&
-                    s.substring(DATA_TAG_POSITION,dataMessage.length()+DATA_TAG_POSITION).equals(dataMessage);
+    private Predicate<String> inputIsDataType;
 
     /**
-     * private constructor of MessageHandler: it's a final class so couldn't exist any object of this.
+     * public constructor of MessageHandler. This class is a singleton
      */
-    private MessageHandler(){
-        throw new AssertionError();
+    MessageHandler(){
+        getMessages();
+        setupBehaviour();
+    }
+
+    /**
+     * This method is used to define handler behaviour when is asked to handle a message received
+     */
+    private void setupBehaviour() {
+        behavioursMap.put(pingMessage, CSMHandler::handlePingMessage);
+        behavioursMap.put(helloMessage, CSMHandler::handleHelloMessage);
+        behavioursMap.put(serverIsReadyMessage, CSMHandler::handleServerIsReadyMessage);
+        inputIsDataType =
+                s->s!=null && s.length()>=dataMessage.length() &&
+                        s.substring(DATA_TAG_POSITION,dataMessage.length()+DATA_TAG_POSITION).equals(dataMessage);
+    }
+
+    /**
+     * This method is used to setup strings used to define the type of messages
+     */
+    private static synchronized void getMessages() {
+        if(!configured){
+            MessageHandlerConfigurations config= ConfigurationHandler.getInstance().getMessageHandlerConfigurations();
+            pingMessage=config.getPingMessage();
+            dataMessage=config.getDataMessage();
+            helloMessage=config.getHelloMessage();
+            charset=config.getCharset();
+            serverIsReadyMessage=config.getServerIsReadyMessage();
+            configured=true;
+        }
     }
 
     /**
@@ -51,7 +73,7 @@ final class MessageHandler {
      * @param input to be computed
      * @exception UndefinedInputTypeException thrown if the input isn't a data nor a defined-type message
      */
-    static void computeInput(SocketConnection connection, String input){
+    void computeInput(SocketConnection connection, String input){
         String data = unBox(input);
         if(inputIsDataType.test(data))
             handleDataInput(connection,data);
@@ -64,7 +86,7 @@ final class MessageHandler {
      * @param connection is the connection who requested to compute an input
      * @param input to be computed
      */
-    private static void handleDataInput(SocketConnection connection,String input) {
+    private void handleDataInput(SocketConnection connection,String input) {
         String data=input.replace(dataMessage,"");
         connection.addToBuffer(data);
         connection.resetTTL();
@@ -76,11 +98,11 @@ final class MessageHandler {
      * @param input to be computed
      * @exception UndefinedInputTypeException thrown if the input isn't a data nor a defined-type message
      * @exception BadSetupException look at:
-     * {@link InputDecoder#handleHelloMessage(SocketConnection)}
-     * {@link InputDecoder#handleServerIsReadyMessage(SocketConnection)}
+     * {@link CSMHandler#handleHelloMessage(SocketConnection)}
+     * {@link CSMHandler#handleServerIsReadyMessage(SocketConnection)}
      */
-    private static void handleOthersInputs(SocketConnection connection, String input) {
-        Optional<DecodingFunction<SocketConnection>> decodingFunction= Optional.ofNullable(INPUT_TYPES.getOrDefault(input, null));
+    private void handleOthersInputs(SocketConnection connection, String input) {
+        Optional<DecodingFunction<SocketConnection>> decodingFunction= Optional.ofNullable(behavioursMap.getOrDefault(input, null));
         decodingFunction.ifPresentOrElse(
                 function->function.run(connection), /*if present*/
                 ()->{throw new UndefinedInputTypeException(); /*if not present*/
@@ -92,7 +114,7 @@ final class MessageHandler {
      * @return a string marked as "DATA TYPE". If this string is sent to another host
      * it will be valuated as a data message.
      */
-    static String computeOutput(String string){
+    String computeOutput(String string){
         StringBuilder stringBuilder= new StringBuilder();
         stringBuilder.append(string);
         stringBuilder.insert(DATA_TAG_POSITION, dataMessage);
@@ -104,7 +126,7 @@ final class MessageHandler {
      * @return a string marked as "DATA TYPE". If this string is sent to another host
      * it will be valuated as a data message.
      */
-    static String computeOutput(int integer){
+    String computeOutput(int integer){
         StringBuilder stringBuilder= new StringBuilder();
         stringBuilder.append(integer);
         stringBuilder.insert(DATA_TAG_POSITION, dataMessage);
@@ -115,7 +137,7 @@ final class MessageHandler {
      * @param data containing data to box
      * @return a string containing data as bytes
      */
-    private static String box(String data){
+    private String box(String data){
         Gson gson= new Gson();
         return gson.toJson(data.getBytes(charset));
     }
@@ -126,7 +148,7 @@ final class MessageHandler {
      * @exception UndefinedInputTypeException is launched if the string received isn't a bytes representation of
      * something
      */
-    private static String unBox(String data){
+    private String unBox(String data){
         Gson gson= new Gson();
         try {
             return new String(gson.fromJson(data, byte[].class), charset);
@@ -139,7 +161,7 @@ final class MessageHandler {
      * Getter for dataTag
      * @return dataTag
      */
-    static String getDataTag(){
+    String getDataTag(){
         return dataMessage;
     }
 
@@ -147,7 +169,7 @@ final class MessageHandler {
      * Getter for dataTagPosition
      * @return dataTagPosition
      */
-    static int getDataTagPosition(){
+    int getDataTagPosition(){
         return DATA_TAG_POSITION;
     }
 
@@ -155,7 +177,7 @@ final class MessageHandler {
      * Getter for predicate "inputIsDataType"
      * @return inputIsDataType
      */
-    static Predicate<String> getInputIsDataType(){
+    Predicate<String> getInputIsDataType(){
         return inputIsDataType;
     }
 
@@ -163,30 +185,30 @@ final class MessageHandler {
      * Getter for pingMessage
      * @return pingMessage
      */
-    static String getPingMessage(){
-        return box(PING_MESSAGE);
+    String getPingMessage(){
+        return box(pingMessage);
     }
 
     /**
      * Getter for helloMessage
      * @return helloMessage
      */
-    static String getHelloMessage() {
-        return box(HELLO_MESSAGE);
+    String getHelloMessage() {
+        return box(helloMessage);
     }
 
     /**
      * Getter for serverIsReadyMessage
      * @return serverIsReadyMessage
      */
-    static String getServerIsReadyMessage() {
-        return box(SERVER_IS_READY_MESSAGE);
+    String getServerIsReadyMessage() {
+        return box(serverIsReadyMessage);
     }
 
     /**
      * @return used charset
      */
-    static Charset getUsedCharset(){
+    Charset getUsedCharset(){
         return charset;
     }
 }

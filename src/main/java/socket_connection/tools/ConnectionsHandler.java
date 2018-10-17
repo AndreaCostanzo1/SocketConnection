83958069a62a01bf359392bf622511c6b4e92802
polimi-hyp-2018-10497583 <andrea1.costanzo@mail.polimi.it>
2018-10-17 20:45:08 +0200

    Created ConnectionsHandler.java: this class is used to handle active connections of a ServerSocketConnection.

---
+++ src/main/java/socket_connection/tools/ConnectionsHandler.java
@@ -0,0 +1,71 @@
+package socket_connection.tools;
+
+import socket_connection.SocketConnection;
+import socket_connection.SocketUserAgentInterface;
+import socket_connection.socket_exceptions.runtime_exceptions.BadSetupException;
+
+import java.util.HashMap;
+import java.util.Optional;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class ConnectionsHandler {
+
+    private Lock availableConnectionsLock;
+    private HashMap<SocketConnection,SocketUserAgentInterface> availableConnections;
+    private int activeConnections;
+
+    /**
+     * Public constructor of ConnectionHandler
+     */
+    public ConnectionsHandler(){
+        availableConnections= new HashMap<>();
+        availableConnectionsLock=new ReentrantLock();
+        activeConnections=0;
+    }
+
+    /**
+     * This method shut down all the active connections
+     */
+    public void shutdownAllConnections(){
+        availableConnectionsLock.lock();
+        availableConnections.values().forEach(SocketUserAgentInterface::shutdown);
+        activeConnections=0;
+        availableConnectionsLock.unlock();
+    }
+
+    /**
+     * Add a connection to the map registering active connections and their relative user agent
+     * @param socket is the socket used to communicate
+     * @param runningAgent is the agent related to the given socket
+     */
+    public void addConnection(SocketConnection socket, SocketUserAgentInterface runningAgent){
+        availableConnectionsLock.lock();
+        availableConnections.put(socket,runningAgent);
+        activeConnections++;
+        availableConnectionsLock.unlock();
+    }
+
+    /**
+     * Remove the connection passed from connections hash map
+     * @param connection to be removed
+     * @exception BadSetupException if the connection isn't in the hash map
+     */
+    public void removeConnection(SocketConnection connection) {
+        availableConnectionsLock.lock();
+        if(!Optional.ofNullable(availableConnections.remove(connection)).isPresent()) throw new BadSetupException();
+        else activeConnections--;
+        availableConnectionsLock.unlock();
+    }
+
+    /**
+     * @return the number of active connection
+     */
+    @SuppressWarnings("WeakerAccess")
+    public int activeConnections(){
+        availableConnectionsLock.lock();
+        int toReturn=activeConnections;
+        availableConnectionsLock.unlock();
+        return toReturn;
+    }
+}

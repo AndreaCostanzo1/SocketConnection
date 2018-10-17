18957a5a39dea4803da180c600f09477b401b44b
polimi-hyp-2018-10497583 <andrea1.costanzo@mail.polimi.it>
2018-10-17 22:17:40 +0200

    Modified ServerSocketCongifurations: added parameters and methods

--- src/main/java/socket_connection/tools/ServerSocketConnectionConfigurations.java
+++ src/main/java/socket_connection/tools/ServerSocketConnectionConfigurations.java
@@ -1,4 +1,24 @@
 package socket_connection.tools;
 
-public class ServerSocketConfigurations {
+import org.jetbrains.annotations.Contract;
+
+public class ServerSocketConnectionConfigurations {
+    private long sleepInMs;
+    private long awaitExecutorInMs;
+
+    ServerSocketConnectionConfigurations(){
+        this.sleepInMs=20;
+        this.awaitExecutorInMs=5000;
+    }
+
+    @Contract(pure = true)
+    public long getSleepInMs() {
+        return sleepInMs;
+    }
+
+
+    @Contract(pure = true)
+    public long getAwaitExecutorInMs() {
+        return awaitExecutorInMs;
+    }
 }
